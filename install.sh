93a68cd6bfb9da75cdb0c54a3339318df886b9b6
Roman Rybalchenko <nerevar@yandex-team.ru>
2013-03-28 17:05:18 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,5 @@ cd ~
 git clone git://github.yandex-team.ru/search-interfaces/dotfiles.git
 chmod 600 ~/dotfiles/ssh/*
 
-# дописываем в самое начало "source dotfiles/..."
-touch ~/.bash_profile
-sed -i '' -e '1i\
-source ~/dotfiles/.bash_profile' ~/.bash_profile
+echo 'source ~/dotfiles/.bash_profile' >> ~/.bash_profile
 source ~/.bash_profile
