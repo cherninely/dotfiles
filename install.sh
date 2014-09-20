47956e36b4bb48508abe7b291beff9edf4f938cc
Igor Novak <vorian@yandex-team.ru>
2014-09-20 17:01:23 +0400

    Fetch neobundle.vim in install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 cd "$(dirname "$0")"
 git pull
 for f in `find . -name '\.*' -exec basename {} \; | grep -v 'git\|\.$'`
@@ -7,3 +8,12 @@ do
 done
 cp .gitconfig ~/.gitconfig
 source ~/.profile
+
+echo Fetch/update neobundle.vim
+rm -rf ~/.vim/bundle/neobundle.vim
+wget -q https://codeload.github.com/Shougo/neobundle.vim/tar.gz/master
+mkdir -p .vim/bundle/
+tar xf master
+mv neobundle.vim-master .vim/bundle/neobundle.vim
+rm master
+echo Done
