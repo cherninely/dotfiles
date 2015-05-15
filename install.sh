c70827740c137846a83476c1f8823d65a17baf0d
Alexander Mekhonoshin <invntrm@yandex-team.ru>
2015-05-15 16:49:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,14 +7,20 @@ do
     ln -fs $PWD/$f ~
 done
 
+# 
+echo Настройка git ...
 GIT_AUTHOR_NAME=`git config --global user.name`
 GIT_AUTHOR_EMAIL=`git config --global user.email`
 cp .gitconfig ~/.gitconfig
 git config --global user.name "$GIT_AUTHOR_NAME"
 git config --global user.email "$GIT_AUTHOR_EMAIL"
 
+#
+echo Перезагрузка bash-профиля ...
 source ~/.profile
 
+# 
+echo Настройка vim ...
 echo Fetch/update neobundle.vim
 rm -rf ~/.vim/bundle/neobundle.vim
 wget -q https://codeload.github.com/Shougo/neobundle.vim/tar.gz/master
@@ -22,4 +28,15 @@ mkdir -p .vim/bundle/
 tar xf master
 mv neobundle.vim-master .vim/bundle/neobundle.vim
 rm master
+
+# OS X specific
+if [[$(uname) = Darwin]]; then
+
+    echo Подготовка wcnew для OS X ...
+    sudo mv /usr/bin/getopt /usr/bin/getopt.bak
+    brew install gnu-getopt
+    ln -s /usr/local/Cellar/gnu-getopt/*/bin/getopt /usr/local/bin/getopt
+
+fi
+
 echo Done
