5ff51e3fe1056f3883f007dff156f3d71d323d6b
Alexander Mekhonoshin (invntrm) <invntrm@yandex-team.ru>
2015-07-11 00:25:14 +0300

    fix migrate-zsh.sh

--- migrate-zsh.sh
+++ migrate-zsh.sh
@@ -5,10 +5,10 @@ echo Установить ZSH если не установлен...
 [[ `which zsh` ]] || sudo apt-get install zsh
 
 # установить или не установить OMZ
-echo 'Ставим OMZ (oh-my-zsh) [yn] ?:'; read WANT_OMZ; [[ $WANT_OMZ == y ]] && (echo 'Ура, Ставим:'; sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"; echo Продолжаем миграцию...) || WANT_OMZ=
+echo 'Ставим OMZ (oh-my-zsh) [yn] ?:'; read WANT_OMZ; [[ $WANT_OMZ == y ]] && (echo 'Ура, Ставим:'; wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -qO- | zsh; echo Продолжаем миграцию...) || WANT_OMZ=
 
 # Если OMZ, то и установить тему ya-mm
-[ $WANT_OMZ ] && (echo Установить тему ya-mm...); cd ~/.oh-my-zsh/themes && wget https://raw.githubusercontent.com/bem-kit/oh-my-zsh/master/themes/ya-mm.zsh-theme)
+[ $WANT_OMZ ] && (echo 'Установить тему ya-mm...'; cd ~/.oh-my-zsh/themes && wget https://raw.githubusercontent.com/bem-kit/oh-my-zsh/master/themes/ya-mm.zsh-theme)
 
 #
 echo заменить .zshrc на симлинк из dotfiles
