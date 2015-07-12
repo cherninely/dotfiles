0b7cf2bf7cd09e8d65661996b3715c856b6e0910
Alexander Mekhonoshin (invntrm) <invntrm@yandex-team.ru>
2015-07-13 01:10:08 +0300

    migrate-zsh.sh: add «wget» dependency resolving

--- migrate-zsh.sh
+++ migrate-zsh.sh
@@ -2,7 +2,10 @@
 
 #
 echo Установить ZSH если не установлен...
-[[ `which zsh` ]] || sudo apt-get install zsh
+which zsh > /dev/null || sudo apt-get install zsh
+
+echo Установить необходимые утилиты
+which wget > /dev/null || sudo apt-get install wget
 
 # установить или не установить OMZ
 echo 'Ставим OMZ (oh-my-zsh) [yn] ?:'; read WANT_OMZ; [[ $WANT_OMZ == y ]] && (echo 'Ура, Ставим:'; wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -qO- | zsh; echo Продолжаем миграцию...) || WANT_OMZ=
