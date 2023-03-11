83c43ba51edcc8245f107e1d559368638f123721
cherninely <cherninely@gmail.com>
2023-03-11 22:45:26 +0300

    telescope layout + ignore tsbuild

--- nvim/lua/plugins/configs/telescope.lua
+++ nvim/lua/plugins/configs/telescope.lua
@@ -5,6 +5,18 @@ if not present then
 end
 
 telescope.setup {
+    defaults = {
+        layout_strategy = "horizontal",
+        layout_config = {
+            horizontal = {
+                preview_width = 0.5,
+                results_width = 0.5,
+            },
+            width = 0.99,
+            height = 0.99,
+            preview_cutoff = 1, -- always show preview
+        },
+    },
     builtin = {
         treesitter = true,
     },
