faf90ea0644d0f7ed09676b34b3b97ca2fee5248
cherninely <cherninely@gmail.com>
2023-08-27 11:57:02 +0300

    finicky + ansible

--- .finicky.js
+++ .finicky.js
@@ -2,7 +2,7 @@ module.exports = {
     defaultBrowser: 'Google Chrome',
     handlers: [
         {
-            match: ['*yandex-team*', '*nda*', '*localhost*', '*figma*', 'zoom.yandex*'],
+            match: ['*yandex-team*', '*nda*', '*localhost*', '*figma*', '*yandex.zoom*'],
             browser: 'Yandex'
         },
     ]
