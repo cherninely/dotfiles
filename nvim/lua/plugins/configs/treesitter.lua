095503861d39e13418d0a91ed92db42a3ff55435
cherninely <cherninely@gmail.com>
2025-10-05 16:51:17 +0300

    Add come small updates

--- nvim/lua/plugins/configs/treesitter.lua
+++ nvim/lua/plugins/configs/treesitter.lua
@@ -5,7 +5,7 @@ end
 
 treesitter.setup {
     -- A list of parser names, or "all" (the four listed parsers should always be installed)
-    ensure_installed = { 'bash', 'vim', 'lua', 'javascript', 'typescript', },
+    ensure_installed = { 'bash', 'vim', 'lua', 'javascript', 'typescript', 'terraform', },
 
     -- Automatically install missing parsers when entering buffer
     -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
